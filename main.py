ca68e133fb19b2617201dc733229f66461f12151
JoeyStark007 <84150889+JoeyStark007@users.noreply.github.com>
2022-11-23 15:25:59 -0500

    Added company logo

--- main.py
+++ main.py
@@ -4,17 +4,28 @@ from backend import get_data
 
 # Add : title, text input, slider, select-box, and sub-header
 
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.write(' ')
+
+with col2:
+    st.image("Visionary.png")
+
+with col3:
+    st.write(' ')
+
 st.title("Joe's Weather Forecast for the Next Days")
 place = st.text_input("Place: In any language ")
 st.write("Please only select US state if its a US city: ")
 state = st.selectbox("Select US state for US city",
-                  ("AL","AK","AZ","AR","AS","CA","CO","CT","DE",
-                   "DC","FL","GA","GU","HI","ID","IL","IN","IA",
-                   "KS","KY","LA","ME","MD","MA","MI","MN","MS",
-                   "MO","MT","NE","NV","NH","NJ","NM","NY","NC",
-                   "ND","MP","OH","OK","OR","PA","PR","RI","SC",
-                   "SD","TN","TX","TT","UT","VT","VA","VI","WA",
-                   "WV","WI","WY"))
+                     ("AL", "AK", "AZ", "AR", "AS", "CA", "CO", "CT", "DE",
+                      "DC", "FL", "GA", "GU", "HI", "ID", "IL", "IN", "IA",
+                      "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS",
+                      "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC",
+                      "ND", "MP", "OH", "OK", "OR", "PA", "PR", "RI", "SC",
+                      "SD", "TN", "TX", "TT", "UT", "VT", "VA", "VI", "WA",
+                      "WV", "WI", "WY"))
 
 days = st.slider("Forecast Days", min_value=1, max_value=5,
                  help="Select the number of forecasted days")
@@ -28,11 +39,11 @@ st.text("Times : 8AM, 11AM, 2PM, 5PM, 8PM, 11PM, 2AM, 5AM")
 data = get_data(place=place, forecast_days=days, state=state)
 
 # create a : temp or sky plot
-filtered_data = get_data(place=place,forecast_days=days)
+filtered_data = get_data(place=place, forecast_days=days)
 if option == "Temperature":
     temp = [dict['main']['temp'] for dict in filtered_data]
     # create list compr for days
-    dates =[dict["dt_txt"] for dict in filtered_data]
+    dates = [dict["dt_txt"] for dict in filtered_data]
     # Temp plot
     figure = px.line(x=dates, y=temp, labels={"x": "Date", "y": "Temperature (C)"})
     st.plotly_chart(figure)
@@ -44,7 +55,3 @@ if option == "Sky":
     # Translate the filepaths with list comprehension
     img_paths = [images[condition] for condition in sky_conditions]
     st.image(img_paths, width=150)
-
-
-
-
